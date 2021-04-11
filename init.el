45574e4f48f440157cea7e2c21af51c1e567cc6e
aabm <aabm@disroot.org>
2021-04-10 21:58:56 -0300

    Add custom functions for window and buffer management

--- init.el
+++ init.el
@@ -11,6 +11,28 @@
 			    (?\[ . ?\]) (?\" . ?\")))
 (electric-pair-mode t)
 
+(defun split-window-below-and-follow ()
+  "A simple replacement for `split-window-below', which automatically focuses the new window."
+  (interactive)
+  (split-window-below)
+  (other-window 1))
+
+(defun split-window-right-and-follow ()
+  "A simple replacement for `split-window-right', which automatically focuses the new window."
+  (interactive)
+  (split-window-right)
+  (other-window 1))
+
+(global-set-key (kbd "C-x 2") 'split-window-below-and-follow)
+(global-set-key (kbd "C-x 3") 'split-window-right-and-follow)
+
+(defun aabm/kill-this-buffer ()
+  "Kill the current buffer. More reliable alternative to `kill-this-buffer'"
+  (interactive)
+  (kill-buffer))
+
+(global-set-key (kbd "C-x k") 'kill-this-buffer)
+
 (with-eval-after-load 'org
   (setq org-src-tab-acts-natively t)
   (setq org-src-fontify-natively t)
