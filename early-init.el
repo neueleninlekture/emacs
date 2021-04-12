d83bcc1fbb6c2cad3d223ce427b37c341597ed47
aabm <aabm@disroot.org>
2021-04-12 18:36:51 -0300

    Fix notify startup: move it to early-init

--- early-init.el
+++ early-init.el
@@ -45,3 +45,10 @@
 
 (setq package-enable-at-startup nil
       package--init-file-ensured t)
+
+(add-hook 'emacs-startup-hook
+	  (lambda ()
+	    (message "Emacs ready in %s with %d garbage collections."
+	     (format "%.2f seconds"
+		     (float-time
+		      (time-subtract after-init-time before-init-time))) gcs-done)))
