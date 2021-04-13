60f958bdaba9b95123bf44909675bd1e3f05b7ce
aabm <aabm@disroot.org>
2021-04-13 11:14:14 -0300

    Restructure window section, add other-window keybinding

--- init.el
+++ init.el
@@ -119,6 +119,8 @@
 (global-set-key (kbd "C-x 2") 'split-window-below-and-follow)
 (global-set-key (kbd "C-x 3") 'split-window-right-and-follow)
 
+(global-set-key (kbd "M-o") 'other-window)
+
 (defun kill-this-buffer+ ()
   "Kill the current buffer. More reliable alternative to `kill-this-buffer'"
   (interactive)
