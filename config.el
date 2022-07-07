da8d520f2c135f758da6b3d02872a95d2e562796
aabm <aabm@disroot.org>
2022-07-06 21:53:56 -0300

    add markdown mode

--- config.el
+++ config.el
@@ -171,6 +171,9 @@ save the Org buffers."
     (org-roam-ui-update-on-save t)
     (org-roam-ui-open-on-start t))
 
+(use-package markdown-mode
+  :straight t)
+
 (use-package org
   :custom
   (org-cite-global-bibliography "~/doc/tex/bib/main.bib"))
